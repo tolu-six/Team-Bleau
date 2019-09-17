54a157d7b9aec436c6320d040476a9840c0d99e1
tolu-six <tolu58@gmail.com>
2019-09-17 18:50:25 +0100

    Updated index.html, comnit datadb.json and signup.php

---
+++ signup.php
@@ -0,0 +1,25 @@
+<?php
+
+if(isset($_POST['signupBtn'])){
+
+	/**File to store user data*/
+	$fileDB = "datadb.json";
+
+	$data = [];
+
+	$formArray = $_POST; #retrieves the form values passed
+	$formArray['password'] = password_hash($formArray['password'], PASSWORD_DEFAULT); #Hash the password
+	
+	$data['id'] = 1;
+	$data = array_merge($data, $formArray); #combine id to the data from form
+
+	#Removes the SignUpBtn since it is returning an empty value
+    unset($data['signupBtn']);
+
+	$json_data = json_encode($data); #Encode data to JSON/Object format
+	
+	/**Every new registration should be appended (i.e added to the next line of the previous)*/
+	file_put_contents($database, $json_data, FILE_APPEND);
+}
+
+?>
