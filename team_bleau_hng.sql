6bab1c280e3acc6d98bde343bb81e04177869e93
tolu-six <tolu58@gmail.com>
2019-09-19 11:45:02 +0100

    initial commit for database

---
+++ team_bleau_hng.sql
@@ -0,0 +1,54 @@
+/*
+SQLyog Community v9.63 
+MySQL - 5.5.5-10.1.37-MariaDB : Database - team_bleau_hng
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`team_bleau_hng` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `team_bleau_hng`;
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) DEFAULT NULL,
+  `user_password_hash` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+/*Data for the table `user` */
+
+insert  into `user`(`id`,`username`,`user_password_hash`,`email`,`created_at`) values (1,'toks','$2y$10$VJVqaDBqvVzicii2y.NRveb3fGIo/NK.0YhF1FRfi4qCdMUelFPe6','test@gmail.com','2019-09-19 11:37:12');
+
+/*Table structure for table `user_profile` */
+
+DROP TABLE IF EXISTS `user_profile`;
+
+CREATE TABLE `user_profile` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `full_name` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+/*Data for the table `user_profile` */
+
+insert  into `user_profile`(`id`,`user_id`,`full_name`,`created_at`) values (1,0,'toks','2019-09-19 11:37:12');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
